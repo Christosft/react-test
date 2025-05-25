7e7cb3baf11da1b315cc94b0039f30b27b9acdef
Chris Ftoulis <christosftoulis3@gmail.com>
2025-05-25 22:47:58 +0300

    feat: background change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout  = ({ children }: LayoutProps) => {
 
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="flex justify-center items-center min-h-screen bg-blue-600">
                 <div className="border border-black rounded-2xl shadow-lg p-6 w-full max-w-3xl min-h-[600px] bg-cover bg-center bg-no-repeat"
                         style={{ backgroundImage: "url('/gal.jpg')" }}>
                     <Header />
